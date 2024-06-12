b1a501a693c60a354a48c54508b8b578b343c5d3
Aldo Joyce <aldousjoyce@gmail.com>
2024-06-12 13:00:17 -0500

    Initial commit

---
+++ source/main.lua
@@ -0,0 +1,127 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+import "CoreLibs/crank"
+import "CoreLibs/ui"
+
+local gfx <const> = playdate.graphics
+local snd <const> = playdate.sound
+local count = 0
+if playdate.file.exists("count.json") then
+    print(playdate.datastore.read("count")[1])
+    count = playdate.datastore.read("count")[1]
+end
+
+local counterSprite = nil
+local buttonSprite = nil
+local armMaskSprite = nil
+
+local numbers = gfx.imagetable.new("assets/images/numbers/num")
+assert(numbers)
+
+local clickSound = snd.sampleplayer.new("assets/click")
+local unclickSound = snd.sampleplayer.new("assets/unclick")
+local dialClickSound = snd.sampleplayer.new("assets/dial_click")
+local dialClickSoundWW = snd.sampleplayer.new("assets/dial_click_wrongway")
+
+local buttonAnimator = gfx.animator.new(1, 200, 200)
+
+local counterSpritesList = {}
+
+local function updateCounter()
+    local countStr = tostring(count)
+    while #countStr < 4 do
+        countStr = "0" .. countStr
+    end
+    for i=1,4 do
+        counterSpritesList[i]:setImage(numbers:getImage(tonumber(countStr:sub(i,i))+1))
+    end
+end
+
+local function setup()
+    counterSprite = gfx.sprite.new(gfx.image.new("assets/images/counter"))
+    counterSprite:moveTo(200, 120)
+    counterSprite:setZIndex(1)
+    counterSprite:add()
+
+    buttonSprite = gfx.sprite.new(gfx.image.new("assets/images/button"))
+    buttonSprite:moveTo(200, 120)
+    buttonSprite:setZIndex(2)
+    buttonSprite:add()
+
+    armMaskSprite = gfx.sprite.new(gfx.image.new("assets/images/arm_mask"))
+    armMaskSprite:moveTo(200, 120)
+    armMaskSprite:setZIndex(3)
+    armMaskSprite:add()
+
+    local countStr = tostring(count)
+    while #countStr < 4 do
+        countStr = "0" .. countStr
+    end
+
+    for i=1,4 do
+        local temp = gfx.sprite.new(numbers:getImage(tonumber(countStr:sub(i,i))+1))
+        temp:setCenter(0,0)
+        temp:moveTo(187 + (i-1)*17, 100)
+        temp:setZIndex(-1)
+        temp:add()
+        table.insert(counterSpritesList, temp)
+    end
+
+    gfx.sprite.setBackgroundDrawingCallback(
+        function(x,y,width,height)
+            gfx.setColor(gfx.kColorBlack)
+            gfx.fillRect(x,y,width,height)
+        end
+    )
+end
+
+function playdate.gameWillTerminate()
+    local cTbl = {count}
+    playdate.datastore.write(cTbl, "count")
+end
+
+playdate.getSystemMenu():addMenuItem("Reset", function()
+    count = 0
+    updateCounter()
+end)
+
+setup()
+
+function playdate.update()
+    if playdate.buttonJustPressed("a") then
+        buttonAnimator = gfx.animator.new(40, 200, 180)
+        clickSound:play(1)
+        count = count + 1
+        if count > 9999 then
+            count = 0
+        end
+        updateCounter()
+    end
+    if playdate.buttonJustReleased("a") then
+        buttonAnimator = gfx.animator.new(40, 180, 200)
+        unclickSound:play(1)
+    end
+
+    buttonSprite:moveTo(buttonAnimator:currentValue(), 120)
+
+    local ticks = playdate.getCrankTicks(9)
+    if ticks == 1 then
+        dialClickSound:play(1)
+        count = (count - (count % 1111))
+        count = count + 1111
+        if count > 9999 then
+            count = 0
+        end
+        updateCounter()
+    elseif ticks == -1 then
+        dialClickSoundWW:play(1)
+    end
+
+    gfx.sprite.update()
+    if playdate.isCrankDocked() then
+        playdate.ui.crankIndicator:draw()
+    end
+    playdate.timer.updateTimers()
+end
